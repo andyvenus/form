7920ba63a0ac4f5fdbc59898044daa80ededdb1b
Andy Venus <andy@eyfs.info>
2020-10-27 15:20:48 +0000

    Split the main functionality of DataStructureForm into a trait
    Allows for easier use in existing forms

--- src/DataStructureForm.php
+++ src/DataStructureForm.php
@@ -3,86 +3,13 @@
 namespace AV\Form;
 
 use AV\Form\DataStructure\DataStructure;
-use AV\Form\DataStructure\Field;
 
 class DataStructureForm extends FormBlueprint
 {
+    use DataStructureFormTrait;
+
     public function __construct(DataStructure $dataStructure)
     {
         $this->addFieldsFromDataStructure($dataStructure);
-    }
-
-    protected function addFieldsFromDataStructure(DataStructure $dataStructure)
-    {
-        foreach ($dataStructure->getFields() as $field) {
-            $type = $this->getInputType($field);
-
-            if ($field->hasDataStructure()) {
-                $this->addFieldsFromDataStructure($field->getDataStructure());
-                continue;
-            }
-
-            $this->add(
-                $this->getInputName($field, $dataStructure->getParentNames()),
-                $type,
-                $this->getInputOptions($field)
-            );
-        }
-    }
-
-    protected function getInputType(Field $field)
-    {
-        if ($inputType = $field->getMetadata('input_type')) {
-            return $inputType;
-        }
-
-        if ($field->hasChoices()) {
-            return 'select';
-        }
-
-        if (in_array($field->getType(), ['float', 'integer', 'double'])) {
-            return 'number';
-        }
-
-        return 'text';
-    }
-
-    protected function getInputName(Field $field, array $parentNames)
-    {
-        if (empty($parentNames)) {
-            $fullName = $field->getName();
-        } else {
-            $fullName = array_shift($parentNames);
-
-            foreach ($parentNames as $parentName) {
-                $fullName .= "[{$parentName}]";
-            }
-
-            $fullName .= "[{$field->getName()}]";
-        }
-
-        if ($field->getType() === 'array' && !$field->hasDataStructure()) {
-            return $fullName.'[]';
-        }
-
-        return $fullName;
-    }
-
-    protected function getInputOptions(Field $field)
-    {
-        $options = [
-            'label' => $field->getLabel()
-        ];
-
-        // Array fields with choices = multiple-select
-        if ($field->hasType('array') && $field->hasChoices()) {
-            $options['attr']['multiple'] = true;
-        }
-
-        if ($field->hasChoices()) {
-            $options['choices'] = $field->getLabelledChoices();
-        }
-
-        return $options;
     }
 }
---
+++ src/DataStructureFormTrait.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace AV\Form;
+
+use AV\Form\DataStructure\DataStructure;
+use AV\Form\DataStructure\Field;
+
+trait DataStructureFormTrait
+{
+    protected DataStructure $dataStructure;
+
+    public function useDataStructure(DataStructure $dataStructure)
+    {
+        $this->dataStructure = $dataStructure;
+    }
+
+    protected function addFieldsFromDataStructure(DataStructure $dataStructure)
+    {
+        $this->useDataStructure($dataStructure);
+
+        foreach ($dataStructure->getFields() as $field) {
+            $this->addFor($field->getName());
+        }
+    }
+
+    protected function addFor(string $fieldName, array $options = [])
+    {
+        if (!isset($this->dataStructure)) {
+            throw new \Exception("You must set a DataStructure using 'useDataStructure' before using 'addFor'");
+        }
+
+        $field = $this->dataStructure->getField($fieldName);
+
+        $type = $options['type'] ?? $this->getInputType($field);
+
+        if ($field->hasDataStructure()) {
+            $this->addFieldsFromDataStructure($field->getDataStructure());
+            return;
+        }
+
+        $this->add(
+            $this->getInputName($field, $this->dataStructure->getParentNames()),
+            $type,
+            $this->getInputOptions($field, $options)
+        );
+    }
+
+    protected function getInputType(Field $field)
+    {
+        if ($inputType = $field->getMetadata('input_type')) {
+            return $inputType;
+        }
+
+        if ($field->hasChoices()) {
+            return 'select';
+        }
+
+        if (in_array($field->getType(), ['float', 'integer', 'double'])) {
+            return 'number';
+        }
+
+        return 'text';
+    }
+
+    protected function getInputName(Field $field, array $parentNames)
+    {
+        if (empty($parentNames)) {
+            $fullName = $field->getName();
+        } else {
+            $fullName = array_shift($parentNames);
+
+            foreach ($parentNames as $parentName) {
+                $fullName .= "[{$parentName}]";
+            }
+
+            $fullName .= "[{$field->getName()}]";
+        }
+
+        if ($field->getType() === 'array' && !$field->hasDataStructure()) {
+            return $fullName.'[]';
+        }
+
+        return $fullName;
+    }
+
+    protected function getInputOptions(Field $field, array $mergeWith = [])
+    {
+        $options = [
+            'label' => $field->getLabel()
+        ];
+
+        // Array fields with choices = multiple-select
+        if ($field->hasType('array') && $field->hasChoices()) {
+            $options['attr']['multiple'] = true;
+        }
+
+        if ($field->hasChoices()) {
+            $options['choices'] = $field->getLabelledChoices();
+        }
+
+        return array_merge_recursive($options, $mergeWith);
+    }
+}
